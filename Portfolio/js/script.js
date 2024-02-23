9a9fa44a410cda54637db966a0f9258525f9fe3d
Felix <54163787+lukifox@users.noreply.github.com>
2024-02-23 15:57:28 +0630

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,7 @@
+/* type script */
+var typed = new Typed(".typing", {
+    strings:["Web Designer", "Web Developer", "Graphic Designer", "Cyber Security Specialist"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
